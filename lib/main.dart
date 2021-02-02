e5f1893bfb228d937823cc058abaee25f9b29cf2
derek-alves <derek.alves1998@hotmail.com>
2021-02-02 00:46:04 -0300

    Leanding Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,60 +22,106 @@ class MyApp extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return WillPopScope(
-        onWillPop: null,
-        child: Container(
-          color: Colors.purple,
-          child: Stack(
-            children: [
-              Container(
-                height: 300,
-                child: Text('ola mundo'),
-              ),
-              Scaffold(
-                backgroundColor: Colors.transparent,
-                appBar: PreferredSize(
-                  preferredSize: Size.fromHeight(150),
-                  child: AppBar(
-                    titleSpacing: 25,
-                    backgroundColor: Colors.purple,
-                    elevation: 0,
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Welcome Back',
-                          style: TextStyle(fontSize: 13),
-                        ),
-                        Text(
-                          'Nestor Jerez',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ],
+      onWillPop: null,
+      child: Container(
+        color: Colors.purple,
+        child: Stack(
+          children: [
+            Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                titleSpacing: 25,
+                backgroundColor: Colors.purple,
+                elevation: 0,
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Welcome Back',
+                      style: TextStyle(fontSize: 13),
                     ),
-                    actions: [
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 12, 0),
-                        child: IconButton(
-                          icon: Icon(Icons.people),
-                          onPressed: () {},
+                    Text(
+                      'Nestor Jerez',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                actions: [
+                  Stack(
+                    children: [
+                      FlatButton(
+                        onPressed: () {},
+                        child: Stack(
+                          children: [
+                            Container(
+                              height: 38,
+                              decoration: BoxDecoration(
+                                  color: Colors.blue[200],
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Image.asset(
+                                "assets/profile.png",
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        left: 22,
+                        bottom: 42,
+                        child: Container(
+                          width: 11,
+                          height: 11,
+                          decoration: BoxDecoration(
+                            color: Colors.red[700],
+                            borderRadius: BorderRadius.circular(50),
+                          ),
                         ),
                       )
                     ],
-                  ),
-                ),
-                body: Container(
-                    height: 500,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[50],
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(50),
-                        topRight: Radius.circular(50),
-                      ),
-                    )),
+                  )
+                ],
               ),
-            ],
-          ),
-        ));
+              body: Stack(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        height: 500,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[50],
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(50),
+                            topRight: Radius.circular(50),
+                          ),
+                        ),
+                        child: null,
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    bottom: 440,
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(23, 0, 0, 0),
+                      height: 120,
+                      width: 310,
+                      child: Card(
+                        color: Color.fromRGBO(130, 172, 255, 1.0),
+                        elevation: 5,
+                        child: Text('Ola mundo'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
